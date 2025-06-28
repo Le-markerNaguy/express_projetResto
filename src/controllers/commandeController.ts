5bc24de8055b05d7907981c2784262372326fc69
Le-markerNaguy <naguyjuccedem@email.com>
2025-06-28 17:31:48 +0200

    Améliration 5

--- src/controllers/commandeController.ts
+++ src/controllers/commandeController.ts
@@ -1,6 +1,5 @@
 import prisma from "../lib/prisma";
 import { Request, Response } from "express";
-import { io } from "../app";
 
 // src/controllers/OrderController.ts
 export const getAllOrders = async (_req: Request, res: Response): Promise<void> => {
@@ -82,6 +81,7 @@ export const createOrder = async (req: Request, res: Response): Promise<void> =>
       }
     });
     // --- EMISSION SOCKET.IO ---
+    const { io } = require("../app");
     io.emit("new-order", newOrder);
     // ---
     res.status(201).json(newOrder);
